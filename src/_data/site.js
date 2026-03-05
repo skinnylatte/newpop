2b9b0b294ef00d6a5354893c63eac583dbe02a42
Adrianna Tan <adrianna@jefritan.com>
2026-03-04 22:49:11 -0800

    replace site.json with dynamic site.js for auto-updating copyright year

---
+++ src/_data/site.js
@@ -0,0 +1,10 @@
+module.exports = {
+  name: "Newpop",
+  url: "https://popagandhi.com",
+  siteTitle: "Popagandhi",
+  authorName: "Adrianna Tan",
+  authorUrl: "https://popagandhi.com/about",
+  description: "punk rock since 2003",
+  socialImage: "/img/202506_whalewatching_whales1.jpg",
+  copyrightYear: new Date().getFullYear().toString(),
+};
