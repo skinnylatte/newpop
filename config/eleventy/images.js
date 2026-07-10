9643240ceb87e78ae08ade030ac6dbf4423aedaf
Adrianna Tan <adrianna@jefritan.com>
2026-07-10 01:44:24 +0100

    drop avif from image formats to fit netlify build timeout
    avif encoding was ~20x the cpu of webp/jpeg (cold build 9min -> 50s local).
    webp+jpeg keeps images 60-80% smaller than originals with universal support.

--- config/eleventy/images.js
+++ config/eleventy/images.js
@@ -3,7 +3,7 @@ const { eleventyImageTransformPlugin } = require("@11ty/eleventy-img");
 function registerImages(eleventyConfig) {
   eleventyConfig.addPlugin(eleventyImageTransformPlugin, {
     extensions: "html",
-    formats: ["avif", "webp", "jpeg"],
+    formats: ["webp", "jpeg"],
     widths: [400, 800, 1200, 1600],
     urlPath: "/img/opt/",
     outputDir: "./_site/img/opt/",
